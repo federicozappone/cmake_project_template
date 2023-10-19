ebeaa4663cc61396365998f342af8a864d8574c3
Federico Zappone <federico.zappone@gmail.com>
2023-10-19 19:39:15 +0200

    Added Doxygen documentation

--- src/division/division.h
+++ src/division/division.h
@@ -1,52 +1,60 @@
-//
-// Created by Konstantin Gredeskoul on 5/14/17.
-//
-
 #ifndef CMAKE_DIVISION_H
 #define CMAKE_DIVISION_H
 
-static const char *const DIVISION_BY_ZERO_MESSAGE = "Division by zero is illegal";
+static const char* const DIVISION_BY_ZERO_MESSAGE = "Division by zero is illegal";
 
 #include <iostream>
 #include <stdexcept>
 
 using namespace std;
 
-class DivisionByZero : public exception {
+class DivisionByZero : public exception
+{
 public:
-  virtual const char *what() const throw() {
-    return DIVISION_BY_ZERO_MESSAGE;
-  }
+  virtual const char* what() const throw() { return DIVISION_BY_ZERO_MESSAGE; }
 };
 
-struct Fraction {
+
+/**
+ * Fraction
+ *
+ * Struct to hold the division parameters.
+ */
+struct Fraction
+{
   long long numerator;
   long long denominator;
 };
 
-struct DivisionResult {
+/**
+ * DivisionResult
+ *
+ * Struct to hold division result.
+ */
+struct DivisionResult
+{
   long long division;
   long long remainder;
 
-  friend bool operator==(const DivisionResult &lhs, const DivisionResult &rhs) {
-    return lhs.division == rhs.division ? lhs.remainder < rhs.remainder : lhs.division < rhs.division;
+  friend bool operator==(const DivisionResult& lhs, const DivisionResult& rhs)
+  {
+    return lhs.division == rhs.division ? lhs.remainder < rhs.remainder
+                                        : lhs.division < rhs.division;
   }
 };
 
-class Division {
+class Division
+{
 public:
-  explicit Division(Fraction fraction) {
-    this->fraction = fraction;
-  }
+  explicit Division(Fraction fraction) { this->fraction = fraction; }
 
-  ~Division() {
-  };
+  ~Division(){};
 
   DivisionResult divide();
 
 protected:
-  Fraction       fraction;
+  Fraction fraction;
   DivisionResult result;
 };
 
-#endif //CMAKE_DIVISION_H
+#endif  // CMAKE_DIVISION_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,18 @@
 
 using namespace std;
 
-static const char *const USAGE =
+static const char* const USAGE =
     "Usage:\n\tdivider <numerator> <denominator>\n\nDescription:\n\tComputes "
     "the result of a fractional division,\n\tand reports both the result and "
     "the remainder.\n";
 
-int main(int argc, const char *argv[]) {
+int main(int argc, const char* argv[])
+{
   Fraction f;
 
   // ensure the correct number of parameters are used.
-  if (argc < 3) {
+  if (argc < 3)
+  {
     cout << USAGE;
     return 1;
   }
@@ -21,14 +23,15 @@ int main(int argc, const char *argv[]) {
   f.denominator = atoll(argv[2]);
 
   Division d = Division(f);
-  try {
+  try
+  {
     DivisionResult r = d.divide();
 
-    cout << "Division : " << f.numerator << " / " << f.denominator << " = "
-         << r.division << "\n";
-    cout << "Remainder: " << f.numerator << " % " << f.denominator << " = "
-         << r.remainder << "\n";
-  } catch (DivisionByZero) {
+    cout << "Division : " << f.numerator << " / " << f.denominator << " = " << r.division << "\n";
+    cout << "Remainder: " << f.numerator << " % " << f.denominator << " = " << r.remainder << "\n";
+  }
+  catch (DivisionByZero)
+  {
     cout << "Can not divide by zero, Homer. Sober up!\n";
   }
   return 0;
